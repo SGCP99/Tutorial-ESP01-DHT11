34e5bc43c438629ed97471521843f1148b3e6a08
SGCP99 <138101851+SGCP99@users.noreply.github.com>
2023-07-10 16:26:11 +0300

    Add files via upload

---
+++ Temp_Hum_AnyWiFi.ino
@@ -0,0 +1,98 @@
+#include <ESP8266WiFi.h>
+#include <DHT.h>
+#include <WiFiManager.h>
+
+#define DHTPIN 2
+#define DHTTYPE DHT11
+
+DHT dht(DHTPIN, DHTTYPE);
+
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+  dht.begin();
+
+  // Uncomment the following line to reset saved WiFi credentials and enter configuration mode
+   WiFiManager wifiManager;
+   wifiManager.resetSettings();
+
+  
+  if (!wifiManager.autoConnect("AP_Name")) {
+    Serial.println("Failed to connect and hit timeout");
+    // Handle the case when Wi-Fi connection fails
+    // You can choose to retry or perform other actions
+  }
+
+  Serial.println("");
+  Serial.println("Wi-Fi connected");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+}
+
+void loop() {
+  WiFiClient client = server.available();
+
+  if (client) {
+    Serial.println("New Client.");
+    String currentLine = "";
+
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-Type: text/html");
+            client.println();
+            client.println("<!DOCTYPE HTML>");
+            client.println("<html>");
+            client.println("<body>");
+            client.print("<h1>DHT11 Sensor Data</h1>");
+            client.print("<p>Temperature: ");
+            client.print(getTemperature());
+            client.print(" &#8451;</p>");
+            client.print("<p>Humidity: ");
+            client.print(getHumidity());
+            client.print(" %</p>");
+            client.println("</body>");
+            client.println("</html>");
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+      }
+    }
+
+    delay(10);
+    client.stop();
+    Serial.println("Client disconnected.");
+  }
+}
+
+float getTemperature() {
+  float temperature = dht.readTemperature();
+
+  if (isnan(temperature)) {
+    Serial.println("Failed to read temperature from DHT sensor!");
+    return 0.0;
+  }
+
+  return temperature;
+}
+
+float getHumidity() {
+  float humidity = dht.readHumidity();
+
+  if (isnan(humidity)) {
+    Serial.println("Failed to read humidity from DHT sensor!");
+    return 0.0;
+  }
+
+  return humidity;
+}
